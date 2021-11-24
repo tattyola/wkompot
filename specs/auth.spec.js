50ee8f7fa525f6d74c0d95b105a9dba0b76c568f
Tatsiana A <astrouskaya@yahoo.com>
2021-11-24 12:26:47 -0500

    add beforeEach hook for auth spec

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -2,8 +2,11 @@ import LoginPage from '../pages/login.page';
 import ProfilePage from '../pages/profile.page';
 
 describe('Aut', function () {
-    it('Successful log in', async function () {
+    beforeEach(async function () {
         await LoginPage.open();
+    });
+
+    it('Successful log in', async function () {
         await expect(LoginPage.buttonSubmit)
             .toBeDisplayed();
         await LoginPage.login('astrouskaya@yahoo.com', '123Green123');
