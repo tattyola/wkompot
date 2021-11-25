aee353f04797323017a6236a4dc8dfbc11e29e47
Tatsiana Astrouskaya <82601614+tattyola@users.noreply.github.com>
2021-11-25 17:09:57 -0500

    add negative tests in auth.spec.js (#4)

--- pages/login.page.js
+++ pages/login.page.js
@@ -1,17 +1,13 @@
 import Page from './page';
 
 class LoginPage extends Page {
-    get inputUsername() {
-        return $('[qa-id="email"]');
-    }
-    
-    get inputPassword() {
-        return $('[qa-id="password"]');
-    }
-    
-    get buttonSubmit() {
-        return $('.login-form-button ');
-    }
+    get inputUsername() {return $('[qa-id="email"]');}
+    get inputPassword() {return $('[qa-id="password"]');}
+    get buttonSubmit() {return $('.login-form-button ');}
+    get notification() {return $('.ant-notification-notice-message');}
+    get loginError() {return $('//div[@data-qa="normalEmail"]//div[@class="ant-form-item-explain-error"]');}
+    get passwordError() {return $('//div[@class="ant-form-item-explain-error"]');}
+    get errorEmailIsNotValid() {return $('//div[@class="ant-form-item-explain-error"][contains(text(), "\'email\' is not a valid email")]')}
 
     async login (username, password) {
         await this.inputUsername.setValue(username);
--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -13,4 +13,35 @@ describe('Aut', function () {
         await expect(ProfilePage.iconUser)
             .toBeDisplayed();
     })
+
+    it('Log in attempt with non-registred email', async function () {
+        await LoginPage.login('invalid@yahoo.com', '123Green123');
+        await expect(LoginPage.notification).toHaveText('Email is not registered');
+    })
+
+    it('Log in attempt with non-registred password', async function () {
+        await LoginPage.login('astrouskaya@yahoo.com', 'invalid');
+        await expect(LoginPage.notification).toHaveText('Incorrect password');
+    })
+
+    it('Credentials are required - uesername', async function () {
+        await LoginPage.inputUsername.setValue('test');
+        await LoginPage.inputUsername.smartClear();
+        await expect(LoginPage.loginError).toHaveText('Required');
+        // await LoginPage.inputPassword.setValue('test');
+        // await LoginPage.inputPassword.smartClear();
+        // await expect(LoginPage.passwordError).toHaveText('Required');
+    })
+
+    it('Credentials are required - password', async function () {
+        await LoginPage.inputPassword.setValue('test');
+        await LoginPage.inputPassword.smartClear();
+        await expect(LoginPage.passwordError).toHaveText('Required');
+    })
+
+    it('Error Email is not valid', async function () {
+        await LoginPage.inputUsername.setValue('astrouskaya');
+        await expect(LoginPage.errorEmailIsNotValid).toHaveText('\'email\' is not a valid email');
+    })
+
 })
